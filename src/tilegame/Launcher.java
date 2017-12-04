5107473dcabfef752854af58fe3f182f1be66270
JohnMichaelWalker <john_walker_333@hotmail.com>
2017-12-04 19:46:11 +0000

    test commit
    changed screen resolution

--- src/tilegame/Launcher.java
+++ src/tilegame/Launcher.java
@@ -3,7 +3,7 @@ package tilegame;
 public class Launcher {
 
 	public static void main(String[] args) {
-		Game game = new Game("Tile Game!", 1024, 768); //640 480
+		Game game = new Game("Tile Game!", 500, 768); //640 480
 		game.start();
 	}
 }
